3acce6201db3fecf327e8710a22efd9192f2f644
Junwoo Kim <junwokim@amazon.com>
2015-06-20 03:45:15 -0700

    Changed the number of arguments for list handler

--- src/sladdit/app/handle_list.clj
+++ src/sladdit/app/handle_list.clj
@@ -2,7 +2,7 @@
   (:require [sladdit.controller.channel-controller :as channel-controller]
             [sladdit.controller.post-controller :as post-controller]))
 
-(defn handle [channel args]
+(defn handle [channel args username]
   (if channel
     (post-controller/get-top-ten)
     "Token is not authorized"))
