a46c6ad81c08900132def501c4791c2df546afa9
Junwoo Kim <junwokim@amazon.com>
2015-06-20 04:12:38 -0700

    Added sqlite db for gitignore

---
+++ db/test-bootstrap.sql
@@ -0,0 +1,5 @@
+insert into channels (channel_name, token, webhook_url) values ('#general', 'pEhHjpChYpPz4Qg3hGssyPvJ', 'https://hooks.slack.com/services/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');
+
+insert into posts (channel_id, submitter, body, submitted_time) values (1, 'junu', 'hello slack', '1434797909');
+
+insert into votes (submitter, post_id, vote_type) values ('junu', 1, 1);
